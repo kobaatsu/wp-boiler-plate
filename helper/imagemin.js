d2f17a16277add4d309a97673e43574395388791
KOBAYASHI atsushi <kobaatsu@gmail.com>
2021-03-27 16:24:22 +0900

    画像素材はどこにおいても良いように変更

--- helper/imagemin.js
+++ helper/imagemin.js
@@ -1,23 +1,17 @@
 const commander = require('commander')
-const imagemin = require('imagemin')
+const imagemin = require('imagemin-keep-folder')
 const imageminGifsicle = require('imagemin-gifsicle')
 const imageminMozjpeg = require('imagemin-mozjpeg')
 const imageminPngquant = require('imagemin-pngquant')
 const imageminSvgo = require('imagemin-svgo')
 const { extendDefaultPlugins } = require('svgo')
-const path = require('path')
 
 const src = [
   `${commander.parse(process.argv).args[0]}/**/*.{jpg,jpeg,gif,png,svg}`,
 ]
-const destination = path.join(
-  __dirname,
-  `../${commander.parse(process.argv).args[1]}`
-)
 
 imagemin(src, {
-  destination,
-  plugins: [
+  use: [
     imageminGifsicle(),
     imageminMozjpeg({ quality: 80 }),
     imageminPngquant({ quality: [0.65, 0.8] }),
